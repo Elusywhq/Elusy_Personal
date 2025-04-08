7626931e8b5154aa61ddf1579fd66f713cb5af8f
Elusys-work-station <Elusywen@icloud.com>
2025-04-08 17:59:19 +0800

    2025-04-08 17:59:19

--- Elusy_packages.sh
+++ Elusy_packages.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-crename() {
-    if [[ $# -ne 2 ]]; then
-        echo "Usage: crename [oldprefix] [newprefix]"
-        return 1
-    fi
 
-    oldprefix="$1"
-    newprefix="$2"
-
-    shopt -s nullglob
-    files=("${oldprefix}"*)
-    shopt -u nullglob
-
-    if [[ ${#files[@]} -eq 0 ]]; then
-        echo "No files found with prefix '${oldprefix}'"
-        return 1
-    fi
-
-    for oldfile in "${files[@]}"; do
-        suffix="${oldfile#$oldprefix}"
-        newfile="${newprefix}${suffix}"
-        cp -i -- "$oldfile" "$newfile"
-    done
-}
 
 
 function PBS-qstat()
