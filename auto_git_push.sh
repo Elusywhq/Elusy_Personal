e7bba1b9e5e9fd546a1a0680b7864c11d175a027
Elusys-work-station <Elusywen@icloud.com>
2025-04-08 13:06:43 +0800

    2025-04-08 13:06:43

---
+++ auto_git_push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    commit_message=$(date +"%Y-%m-%d %H:%M:%S")
+else
+    commit_message=$1
+fi
+
+git add .
+git commit -m "$commit_message"
+git push origin main
