a258c0e852d6270b1e4d05ebcdb39750a176946a
root <root@SCI-F3V0WV34.stf.nus.edu.sg>
2025-04-10 15:24:51 +0800

    2025-04-10 15:24:51

--- Elusy_packages.sh
+++ Elusy_packages.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [[ $(hostname) == *"atlas"* ]]; then
+  Host="hpc"
+  scratch=/hpctmp/e0732532
+  echo "atlas"
+  echo $scratch
+elif [[ $(hostname) == *"asp2a"* ]]; then
+  Host="nscc"
+  scratch=/home/users/nus/$USER
+  echo "nscc"
+elif [[ $(hostname) == *"Precision"* ]]; then
+  Host="bohr"
+  scratch=/home/scratch/hanqi/orca
+  echo "bohr"
+fi
 
 
 
@@ -14,6 +28,17 @@ function PBS-host()
     for id in "$@"; do
         qstat -f "$id" | grep host
     done
+}
+
+function PBS-chmlu5()
+{
+    if [ "$Host" = "hpc" ]; then
+        for file in "$@"; do
+           file_name=${file%.*}
+           sed -i "s/colo-chmlu-08/colo-chmlu-05/g" "$file_name.sh"
+        done
+    fi
+
 }
 
 function qjlist_test(){
