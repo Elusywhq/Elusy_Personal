4b7a61235e56dbdf5cb379e7b6f2d0f75d43acba
root <root@SCI-F3V0WV34.stf.nus.edu.sg>
2025-05-16 17:00:25 +0800

    2025-05-16 17:00:25

--- Elusy_packages.sh
+++ Elusy_packages.sh
@@ -63,26 +63,3 @@ function PBS-chmlu4()
 
 }
 
-function qjlist_test(){
-    if [ -n "$1" ]; then
-        qstat -f | grep -E "Job Id|Job_Name" | awk '
-            BEGIN { ORS="\t" } 
-            /Job Id/ { printf "\n%-15s\t", substr($3, 1, 15) } 
-            /Job_Name/ { job_name = $3 } 
-            END { print job_name }' | while read -r line; do
-                job_id=$(echo "$line" | awk '{print $1}')
-                job_stat=$(qstat -fx "$job_id" | grep job_stat | awk "{print \$3}")
-                echo -e "$line\t$job_stat"
-            done | grep "$1"
-    else
-        qstat -f | grep -E "Job Id|Job_Name" | awk '
-            BEGIN { ORS="\t" } 
-            /Job Id/ { printf "\n%-15s\t", substr($3, 1, 15) } 
-            /Job_Name/ { job_name = $3 } 
-            END { print job_name }' | while read -r line; do
-                job_id=$(echo "$line" | awk '{print $1}')
-                job_stat=$(qstat -fx "$job_id" | grep job_stat | awk "{print \$3}")
-                echo -e "$line\t$job_stat"
-            done
-    fi
-}
